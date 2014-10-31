929a5113777a82bbbb26bdee37f4409a3891032d
conn6070 <conn6070@ISS147D8-03.scdsb.pri>
2014-10-31 09:18:18 -0400

    Lesson 8git

--- src/Student.java
+++ src/Student.java
@@ -6,7 +6,7 @@ public class Student {
     	private String name;
     
 	//turn this into an array///////////////
-    	private int mark1, mark2, mark3; 
+    	private int mark[]= new int [3]; 
 
 
  //constructor methods - 3 ways you can make a student
@@ -18,14 +18,14 @@ public class Student {
 	//Additonal Constructor
 	public Student(String nm, int t1, int t2, int t3) {
 		name = nm;
-		mark1 = t1;
-		mark2 = t2;
-		mark3 = t3;
+		mark[0] = t1;
+		mark[1] = t2;
+		mark[2] = t3;
 	}
 
 	//Another constructor
 	public Student(Student s) {
-		this(s.name, s.mark1, s.mark2, s.mark3);
+		this(s.name, s.mark[0], s.mark[1], s.mark[2]);
 	}
 
     /*
@@ -40,44 +40,44 @@ public class Student {
 
     public void setMark(int whichmark, int number){
     	if (whichmark == 1) {
-    		mark1 = number;
+    		mark[0] = number;
     	} else if (whichmark == 2) {
-    		mark2 = number;
+    		mark[1] = number;
     	} else{
 
-    		mark3 = number;
+    		mark[2] = number;
     	}
     }
 
     public int getAverage() {
-    	int average = (mark1 + mark2 + mark3) / 3;
+    	int average = (mark[0] + mark[1] + mark[2]) / 3;
     	return average;
     }
 
 
     public int getMark(int whichmark) {
-	if (whichmark == 1) return mark1;
-	else if (whichmark ==2) return mark2;
-	else return mark3;
+	if (whichmark == 1) return mark[0];
+	else if (whichmark ==2) return mark[1];
+	else return mark[2];
     }
 
     public int getHighscore() {
-    	if (mark1 > mark2 && mark1 > mark3) {
-    		return mark1;
+    	if (mark[0] > mark[1] && mark[0] > mark[2]) {
+    		return mark[0];
     	}
-    	else if (mark2 > mark1 && mark2 > mark3){
-    		return mark2;
+    	else if (mark[1] > mark[0] && mark[1] > mark[2]){
+    		return mark[3];
 
     	} else {
-    		return mark3;
+    		return mark[2];
     	}
     }
 
     public String toString() {
     	String result = "Name: " + name;
-    	result += "\nMark 1:\t" + mark1;
-    	result += "\nMark 2:\t" + mark2;
-    	result += "\nMark 3:\t" + mark3;
+    	result += "\nMark 1:\t" + mark[0];
+    	result += "\nMark 2:\t" + mark[1];
+    	result += "\nMark 3:\t" + mark[2];
     	result += "\n~~~~~~~~~~~~~~~~~~";
     	result += "\nAverage:\t" + getAverage();
     	return result;
@@ -94,7 +94,7 @@ public class Student {
     	if (name.equals("")) 
     		message += "\nName is required\nPlease re-enter all data";
     	
-	if (mark1 < 0 || mark1 > 100 || mark2 < 0 || mark2 > 100 || mark3 < 0 || mark3 > 100) 
+	if (mark[0] < 0 || mark[0] > 100 || mark[1] < 0 || mark[1] > 100 || mark[2] < 0 || mark[2] > 100) 
     		message += "\nAt least one mark is out of range, please re-enter all data";
 	
 	return message;
