e222e62ecb91a67470aaa9ae9023da7b1f8e4af6
Dell <Dell@Dell-PC>
2014-11-17 00:18:14 -0500

    craps

--- src/CrapsGUI.java
+++ src/CrapsGUI.java
@@ -2,9 +2,14 @@ import javax.swing.ImageIcon;
 import java.net.URL;
 public class CrapsGUI extends javax.swing.JFrame {
 
-  
+    int $;
+    Craps game;
+    boolean newg;
     public CrapsGUI() {
         initComponents();
+    newg = true;
+    game = new Craps();
+    
     }
 
   
@@ -13,7 +18,13 @@ public class CrapsGUI extends javax.swing.JFrame {
     private void initComponents() {
 
         btnroll = new javax.swing.JButton();
-        lbldie = new javax.swing.JLabel();
+        lbldie1 = new javax.swing.JLabel();
+        lbldie2 = new javax.swing.JLabel();
+        btnquit = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txtgame = new javax.swing.JTextArea();
+        lblmoney = new javax.swing.JLabel();
+        lbltotal = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -24,37 +35,90 @@ public class CrapsGUI extends javax.swing.JFrame {
             }
         });
 
-        lbldie.setText("jLabel1");
-        lbldie.setBorder(new javax.swing.border.MatteBorder(null));
+        lbldie1.setBorder(new javax.swing.border.MatteBorder(null));
+
+        btnquit.setText("Quit");
+
+        txtgame.setColumns(20);
+        txtgame.setRows(5);
+        jScrollPane1.setViewportView(txtgame);
+
+        lblmoney.setText("$100");
+
+        lbltotal.setText("TOTAL");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(41, 41, 41)
-                .addComponent(btnroll)
-                .addGap(77, 77, 77)
-                .addComponent(lbldie, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(120, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(30, 30, 30)
+                        .addComponent(lbldie1, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(37, 37, 37)
+                        .addComponent(lbldie2, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 20, Short.MAX_VALUE)
+                        .addComponent(lbltotal, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(41, 41, 41)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 235, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(btnroll)
+                                .addGap(45, 45, 45)
+                                .addComponent(btnquit)
+                                .addGap(41, 41, 41)
+                                .addComponent(lblmoney, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addGap(55, 55, 55))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(43, 43, 43)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(lbldie1, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(lbldie2, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(79, 79, 79)
+                        .addComponent(lbltotal, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lbldie, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnroll))
-                .addContainerGap(144, Short.MAX_VALUE))
+                    .addComponent(btnroll)
+                    .addComponent(btnquit)
+                    .addComponent(lblmoney, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(30, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnrollActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnrollActionPerformed
-int num=(int)(Math.random()*6)+1;       
-URL file=getClass().getResource("d"+num + ".JPG");
-lbldie.setIcon(new ImageIcon(file));
+
+
+   //roll both die assume our Craps game is called 'game'
+        game.roll();
+        //display die images
+        lbldie1.setIcon(game.getDiePic(1));
+        lbldie2.setIcon(game.getDiePic(2));
+       
+        if(newgame){
+            
+            txtgame.setText("New Game\n----\nYou rolled a: "+game.getTotal());
+            newgame=false; //make it so its not a new game anymore
+            money-=5; //charge 5 dollars for new game
+            lblmoney.setText("$ "+money);//update money display
+        }
+        else 
+            txtgame.append("\nYou rolled a: "+game.getTotal());
+        
+        //update big total number at the side
+        lblmoney.setText(" "+game.getTotal());
+        //determine if game.hasWon() or game.hasLost()
     }//GEN-LAST:event_btnrollActionPerformed
 
     public static void main(String args[]) {
@@ -90,7 +154,13 @@ lbldie.setIcon(new ImageIcon(file));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnquit;
     private javax.swing.JButton btnroll;
-    private javax.swing.JLabel lbldie;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lbldie1;
+    private javax.swing.JLabel lbldie2;
+    private javax.swing.JLabel lblmoney;
+    private javax.swing.JLabel lbltotal;
+    private javax.swing.JTextArea txtgame;
     // End of variables declaration//GEN-END:variables
 }
--- src/Die.java
+++ src/Die.java
@@ -1,3 +1,7 @@
+
+import java.net.URL;
+import javax.swing.ImageIcon;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +13,31 @@
  * @author conn6070
  */
 public class Die {
+   
+    private ImageIcon pic;
+    private int value; 
+    public Die()
+    {
+        value = 1;
+        URL file = getClass().getResource("d1.jpg");
+        pic = new ImageIcon(file);
+    }
     
+  
+    
+    public void roll()
+    {
+        value = (int)(Math.random()*6)+1;
+        URL file = getClass().getResource("d" + value + ".jpg");
+        pic = new ImageIcon(file);
+    }
+    
+    public ImageIcon getPic()
+    {
+        return pic;
+    } 
+      public int getValue()
+    {
+        return value;
+    }
 }
